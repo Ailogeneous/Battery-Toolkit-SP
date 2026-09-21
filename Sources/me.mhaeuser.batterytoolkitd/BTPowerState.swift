2cfe78db574a1961ec3ee400dff205be1faba1d9
InconsequentialGene <54972175+HeyimYev@users.noreply.github.com>
2026-09-20 22:34:28 -0700

    Revert "Force charge-enable SMC write"
    This reverts commit 77c0d29e980f4f2b3aae5025a4fea21c58a771da.

--- Sources/me.mhaeuser.batterytoolkitd/BTPowerState.swift
+++ Sources/me.mhaeuser.batterytoolkitd/BTPowerState.swift
@@ -154,6 +154,10 @@ internal enum BTPowerState {
     }
 
     static func enableCharging(percent: UInt8) -> Bool {
+        guard self.chargingDisabled else {
+            return true
+        }
+
         let success = SMCComm.Power.enableCharging()
         if !success {
             os_log("Failed to enable charging")
